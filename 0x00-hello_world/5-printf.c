ed8f3eb6969fe07dc52c652b1aafb0f886d83681
Yahyah Badmus <badmusyahyah@gmail.com>
2022-04-07 10:06:16 -0700

    standard output with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * Description: main - program start from main function
+ *
+ * Return: always return 0 for successful execution
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
