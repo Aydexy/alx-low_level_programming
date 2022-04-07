9ab9451f33003288299f1140272554a4b8567803
Yahyah Badmus <badmusyahyah@gmail.com>
2022-04-07 13:53:09 -0700

    101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - starting point
+ *
+ * Return: 0 valve must be returned
+ */
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19"\n);
+	return (1);
+}
