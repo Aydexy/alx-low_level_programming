56b4cf497e19915f8801ed6cc90c21a28aa23608
Yahyah Badmus <badmusyahyah@gmail.com>
2022-04-07 09:50:04 -0700

    standard output with puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * Description: main - It prints out the standard out.
+ * Return: 0 if succesful
+ */
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
