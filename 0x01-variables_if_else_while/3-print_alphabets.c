3ba61c1dbdade14aecbfd164d1b8c0d8e10ae35f
Yahyah Badmus <badmusyahyah@gmail.com>
2022-04-08 16:38:35 -0700

    Alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/** main: Alphabet in lowercase and uppercase
+ *
+ * Return: 0 value
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
