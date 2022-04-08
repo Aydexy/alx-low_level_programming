77ee2b35a576746bec24ab4b6322d8e338f9e545
Yahyah Badmus <badmusyahyah@gmail.com>
2022-04-08 16:24:49 -0700

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * Description: main - loop from a to z
+ *
+ * Return: 0 value return on success
+ */
+int main(void)
+{
+	char c;
+
+	for (c = "a"; c <= "z"; c++)
+		putchar(c);
+	putchar("\n");
+
+	return (0);
+}
