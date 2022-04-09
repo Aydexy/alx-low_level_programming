b54abe2988e854fc0015341310231d1fb76717e7
Yahyah Badmus <badmusyahyah@gmail.com>
2022-04-08 17:42:36 -0700

    Print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * Description: main -  print numbers
+ *
+ * Return: 0 value must be return on success
+ */
+int main(void)
+{
+	int  n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar( n);
+	}
+	putchar('\n');
+
+	return (0);
+}
