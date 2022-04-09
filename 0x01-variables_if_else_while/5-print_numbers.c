3f7aa8546c85f0f072f21fbf889a741315f089a4
Yahyah Badmus <badmusyahyah@gmail.com>
2022-04-08 17:18:37 -0700

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * Description: main - print 0 to 9
+ *
+ * Return: 0 value
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+
+	return (0);
+}
