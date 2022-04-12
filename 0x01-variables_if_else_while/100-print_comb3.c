87b9ed817ae2640a1b2b5b834692284cfdb006ae
Yahyah Badmus <badmusyahyah@gmail.com>
2022-04-11 19:18:18 -0700

    different combinations of two digits.

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,35 +1,36 @@
 #include <stdio.h>
-
 /**
- * main - program that prints all possible different combinations of two digits
- * Numbers must be separated by ,followed by a space
- * The two digits must be different
- * 01 and 10 are considered the same combination of the two digits 0 and 1
- * Return: 0
+ *Description:  main - print numbers from 00 to 99.
+(*
+ * Return: value 0 must return  on success
  */
-
 int main(void)
 {
-	int n1 = 48;
-	int n2;
-	int comb = 44;
+	int x = '0';
+	int y = '0';
 
-	while (n1 <= 57)
+	while (x <= '9')
 	{
-		n2 = n1 + 1;
-		while (n2 <= 57)
-	{
-		putchar(n1);
-		putchar(n2);
-		if (n1 != 56 || n2 != 57)
-	{
-		putchar(comb);
-		putchar(32);
+		while (y <= '9')
+		{
+			if (x < y || x != y)
+			{
+				putchar(x);
+				putchar(y);
+				if (x == '8' && y == '9')
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			y++;
+		}
+		y = '0';
+		x++;
 	}
-		n2 += 1;
-	}
-		n1 += 1;
-		putchar('\n');
-	}
-		return (0);
+	return (0);
 }
