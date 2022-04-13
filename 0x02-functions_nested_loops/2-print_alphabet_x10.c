011ca81e47a1cfa989a2f6ff20b0b122f04896d1
Yahyah Badmus <badmusyahyah@gmail.com>
2022-04-13 07:02:51 -0700

    10 times the alphabet, in lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - prints 10 times the alphabet, in lowercase, followed by a new line.
+ * main - print_alphabet_x10
  * Description: You are not allowed to include standard libraries.
  * Return: 0 value on success
  */
