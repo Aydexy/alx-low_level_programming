a435fb428e23d430bc4423a8258de3e28e296d6d
Yahyah Badmus <badmusyahyah@gmail.com>
2022-04-13 04:36:42 -0700

    _putchar task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * Decsription: main - print _putchar
+ *
+ * Return - 0 value on success
+ */
+int main(void)
+{
+	_putchar("_putchar");
+	_putchar("\n");
+
+	return (0);
+}
