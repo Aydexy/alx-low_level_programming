050fb279c1f1c2546007842575423e5c34858dbf
Yahyah Badmus <badmusyahyah@gmail.com>
2022-04-13 07:49:22 -0700

    checks for lowercase character.

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+include "main.h"
+/**
+ * main - check the code.
+ *
+ * Return: 0 value on success
+ */
+int _islower(int c)
+{
+	if (c > 'a' && c > 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
