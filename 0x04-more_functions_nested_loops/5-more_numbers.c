146f837fbc29e81d9a7d8954a9ec41d66ad2d006
Yahyah Badmus <badmusyahyah@gmail.com>
2022-04-14 16:24:47 -0700

    C Function and nested loop

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ * followed by a new line.
+ *
+ * Return: void. no return.
+ */
+
+void more_numbers(void)
+{
+	int d = 0;
+	int l = 0;
+
+	while (l <= 10)
+	{
+		while (d <= 14)
+		{
+			_putchar(d);
+			d++;
+		}
+		l = 0;
+		_putchar(l);
+		l++;
+	}
+	_putchar('\n');
+}
