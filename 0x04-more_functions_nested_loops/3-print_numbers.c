182a07feb781f4718fabb90e3a6a4dc4d37231b9
Yahyah Badmus <badmusyahyah@gmail.com>
2022-04-14 14:40:55 -0700

    C Function and nested loop

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
 * print_numbers - prints 0-9 with a new line
@@ -14,6 +13,6 @@ void print_numbers(void)
 	for (i = 0; c <= 9; c++)
 	{
 		_putchar(i);
-		-putchar('\n');
 	}
+	_putchar('\n');
 }
