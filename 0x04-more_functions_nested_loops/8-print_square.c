c466201eaa2e702aaad50f64c07092f4e48408cf
Yahyah Badmus <badmusyahyah@gmail.com>
2022-04-14 20:21:32 -0700

    C Function and nested loop

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+/**
+* print_square - prints a size by size square
+* @size: an int given by main
+*
+* Return: void. no return.
+*/
+
+void print_square(int size)
+{
+	int i, j;
+
+
+	if (size <= 0)
+		_putchar('\n');
+	for (i = size; i > 0; i--)
+	{
+		for (j = size; j > 0; j--)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
