98f64ef90cea8748f7383e2666d024b4ba910b0d
Yahyah Badmus <badmusyahyah@gmail.com>
2022-04-14 20:34:26 -0700

    C Function and nested loop

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle, followed by a new line.
+ *
+ * Return: 0 value on success
+ */
+int main(void)
+{
+    print_triangle(2);
+    print_triangle(10);
+    print_triangle(1);
+    print_triangle(0);
+    return (0);
+}
