525616459e5d70eecc3e40a6ab83852d047a689d
Yahyah Badmus <badmusyahyah@gmail.com>
2022-05-27 21:30:51 -0700

    more Linked_lists

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -10,12 +10,13 @@ int sum_listint(listint_t *head)
 
 	while (head)
 	{
+		/* find the sum of nodes */
 		sumofnodes(head, &sum);
+		/* required sum */
 		return (sum);
 	}
 	return (0);
 }
-
 /**
  * sumofnodes - find the sum of ndoes
  * @head: a pointer to the head of the lintint_t list
@@ -24,14 +25,17 @@ int sum_listint(listint_t *head)
  * of the given linked list
  * Return: the sum
  */
-
 void sumofnodes(listint_t *head, int *sum)
 {
 	int n;
-
+	/* if head is equal to NULL */
 	if (!head)
 		return;
+	/* de reference to n to the data */
+	/* so I can make the sum */
 	n = head->n;
+	/* recursively traverse the remaining nodes  */
 	sumofnodes(head->next, sum);
+	/* accumulate sum */
 	*sum = *sum + n;
 }
